9c66f13a163274e36f9bda294e032f3080867ad9
anne <richardson.ae@gmail.com>
2015-11-21 12:29:50 -0600

    added transportation drop down to form

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,3 +1,7 @@
 class Rsvp < ActiveRecord::Base
   belongs_to :rsvpable, polymorphic: true
+
+  TRANSPORTATION = ["will drive", "needs ride", "has car , prefers to ride"]
+
+  validates :transportation, inclusion: { in: TRANSPORTATION }
 end #RSVP
